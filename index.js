5c5f2b1ad2f9b7564a75d32656009551fbe7824f
Brian Dillingham <brian@dillingham.dev>
2022-02-01 17:39:41 -0500

    Add more extensions

--- index.js
+++ index.js
@@ -1,52 +1,52 @@
 // layouts
-import SidebarLayout from "./src/Layouts/SidebarLayout";
+import SidebarLayout from "./src/Layouts/SidebarLayout.vue";
 // items
-import Table from './src/Items/Table';
-import Grid from './src/Items/Grid';
-import List from './src/Items/List';
-import Empty from './src/Items/Empty';
+import Table from './src/Items/Table.vue';
+import Grid from './src/Items/Grid.vue';
+import List from './src/Items/List.vue';
+import Empty from './src/Items/Empty.vue';
 // elements
-import Importer from './src/Elements/Importer';
-import Seeker from './src/Elements/Seeker';
-import Topper from './src/Elements/Topper';
-import ItemDots from './src/Elements/ItemDots';
-import Form from './src/Elements/Form';
-import Detail from './src/Elements/Detail';
-import Finder from './src/Elements/Finder';
-import Switch from './src/Elements/Switch';
-import Tabs from './src/Elements/Tabs';
-import Heading from './src/Elements/Heading';
-import Notifications from './src/Elements/Notifications';
-import Pagination from './src/Elements/Pagination';
-import Dropdown from './src/Elements/Dropdown';
-import Timestamp from './src/Elements/Timestamp';
-import Breadcrumbs from './src/Elements/Breadcrumbs';
-import Flash from './src/Elements/Flash';
-import DateInterval from './src/Inputs/DateInterval';
+import Importer from './src/Elements/Importer.vue';
+import Seeker from './src/Elements/Seeker.vue';
+import Topper from './src/Elements/Topper.vue';
+import ItemDots from './src/Elements/ItemDots.vue';
+import Form from './src/Elements/Form.vue';
+import Detail from './src/Elements/Detail.vue';
+import Finder from './src/Elements/Finder.vue';
+import Switch from './src/Elements/Switch.vue';
+import Tabs from './src/Elements/Tabs.vue';
+import Heading from './src/Elements/Heading.vue';
+import Notifications from './src/Elements/Notifications.vue';
+import Pagination from './src/Elements/Pagination.vue';
+import Dropdown from './src/Elements/Dropdown.vue';
+import Timestamp from './src/Elements/Timestamp.vue';
+import Breadcrumbs from './src/Elements/Breadcrumbs.vue';
+import Flash from './src/Elements/Flash.vue';
+import DateInterval from './src/Inputs/DateInterval.vue';
 // metrics
-import Cards from "./src/Cards/Cards";
+import Cards from "./src/Cards/Cards.vue";
 // query
-import QueryDropdown from './src/Query/Dropdown';
-import QueryGroup from './src/Query/Group';
-import QueryRange from './src/Query/Range';
+import QueryDropdown from './src/Query/Dropdown.vue';
+import QueryGroup from './src/Query/Group.vue';
+import QueryRange from './src/Query/Range.vue';
 // details
-import DetailText from './src/Details/Text';
+import DetailText from './src/Details/Text.vue';
 // inputs
-import Picker from './src/Inputs/Picker';
-import Select from './src/Inputs/Select';
-import Textarea from './src/Inputs/Textarea';
-import ImageInput from './src/Inputs/Image';
-import FileInput from './src/Inputs/File';
+import Picker from './src/Inputs/Picker.vue';
+import Select from './src/Inputs/Select.vue';
+import Textarea from './src/Inputs/Textarea.vue';
+import ImageInput from './src/Inputs/Image.vue';
+import FileInput from './src/Inputs/File.vue';
 // mixins
-import Headless from './src/Mixins/Headless';
-import Items from './src/Mixins/Items';
-import Connect from './src/Mixins/Connect';
-import Filtering from './src/Mixins/Filtering';
-import ToggleFilters from './src/Mixins/ToggleFilters';
-import ToggleSidebar from './src/Mixins/ToggleSidebar';
-import ClickOutside from './src/Mixins/ClickOutside';
-import Permissions from './src/Mixins/Permissions';
-import GetRoute from './src/Mixins/GetRoute';
+import Headless from './src/Mixins/Headless.js';
+import Items from './src/Mixins/Items.js';
+import Connect from './src/Mixins/Connect.js';
+import Filtering from './src/Mixins/Filtering.js';
+import ToggleFilters from './src/Mixins/ToggleFilters.js';
+import ToggleSidebar from './src/Mixins/ToggleSidebar.js';
+import ClickOutside from './src/Mixins/ClickOutside.js';
+import Permissions from './src/Mixins/Permissions.js';
+import GetRoute from './src/Mixins/GetRoute.js';
 
 import ConnectedComponents from './src/Classses/ConnectedComponents';
 import DataWrapper from './src/Classses/DataWrapper';
