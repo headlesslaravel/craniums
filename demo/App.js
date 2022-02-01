07deb956cc85fb4f9105be051190c71f1e138d1c
Brian Dillingham <brian@dillingham.dev>
2022-02-01 17:48:21 -0500

    Add tailwind for demo

--- demo/App.js
+++ demo/App.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import Cranium from '../index.js'
+import './app.css'
 
 createApp(App)
     .mixin(Cranium)
--- demo/App.vue
+++ demo/App.vue
@@ -5,16 +5,6 @@
     />
 </template>
 
-<style>
-#app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-}
-</style>
 <script>
 import Table from "../src/Items/Table.vue";
 export default {
