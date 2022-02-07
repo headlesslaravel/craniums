b078af36699099670f792371e737a4ccb202ffd2
Brian Dillingham <brian@dillingham.dev>
2022-02-07 12:15:41 -0500

    Use vue-popper for dropdown

--- src/Elements/Dropdown.vue
+++ src/Elements/Dropdown.vue
@@ -1,53 +1,24 @@
 <template>
-    <div v-click-outside="close" class="relative z-50" data-test="cranium-dropdown">
-        <button ref="trigger" data-test="cranium-dropdown-trigger" @click="toggle" class="cursor-pointer">
+    <div data-test="cranium-dropdown" class="z-10">
+      <Popper>
+        <button data-test="cranium-dropdown-trigger">
             <slot name="trigger"></slot>
         </button>
-
-        <teleport to="body">
-            <div v-show="open" class="absolute top-0 left-0 z-50" data-test="cranium-dropdown-content">
-                <div
-                    ref="content"
-                    :style="{ top: top + 'px', left: left + 'px', width: width + 'px' }"
-                    class="relative mt-1 rounded-md shadow-lg z-50"
-                >
-                    <div
-                        class="bg-white rounded-md ring-1 ring-black ring-opacity-5 z-50"
-                        @click.stop
-                    >
-                        <slot name="content"></slot>
-                    </div>
-                </div>
-            </div>
-        </teleport>
+        <template #content>
+          <div class="bg-white rounded shadow-lg p-5" data-test="cranium-dropdown-content">
+            <slot name="content"></slot>
+          </div>
+        </template>
+      </Popper>
     </div>
 </template>
 
 <script>
+import Popper from "vue3-popper";
+
 export default {
-    props: {
-        width: {
-            type: Number,
-            default: 240,
-        },
-    },
-    data() {
-        return {
-            open: false,
-            top: 0,
-            left: 0,
-        };
-    },
-    methods: {
-        toggle() {
-            let trigger = this.$refs.trigger.getBoundingClientRect();
-            this.top = trigger.top + trigger.height;
-            this.left = trigger.left - this.width + trigger.width;
-            this.open = !this.open;
-        },
-        close() {
-            this.open = false;
-        },
+    components: {
+      Popper,
     },
 };
 </script>
